04ded65d61f0296c29b6fe7aea853eb4a20034b0
Suslov Danil <danya.sus@yandex.ru>
2022-06-22 19:26:41 +0300

    Добавьте файлы проекта.

---
+++ IProcessService.cs
@@ -0,0 +1,11 @@
+﻿using SelfHostedServer.Models.Entities;
+using System.Threading.Tasks;
+
+namespace SelfHostedServer.Services
+{
+    public interface IProcessService
+    {
+        Task SaleAsync(Ticket ticket);
+        Task RefundAsync(Refund refund);
+    }
+}
---
+++ ProcessService.cs
@@ -0,0 +1,42 @@
+﻿using SelfHostedServer.Data;
+using SelfHostedServer.Models.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace SelfHostedServer.Services
+{
+    public class ProcessService : IProcessService
+    {
+        private readonly ITicketRepository Repository;
+        public ProcessService(ITicketRepository repository)
+        {
+            this.Repository = repository;
+        }
+
+        public async Task SaleAsync(Ticket ticket)
+        {
+            var delay = Task.Delay(1200000);
+
+            var result = Repository.SaleAsync(ticket);
+
+            if (await Task.WhenAny(result, delay) != delay)
+            {
+                await result;
+            }
+            else throw new TimeoutException();
+        }
+
+        public async Task RefundAsync(Refund refund)
+        {
+            var delay = Task.Delay(1200000);
+
+            var result = Repository.RefundAsync(refund);
+
+            if (await Task.WhenAny(result, delay) != delay)
+            {
+                await result;
+            }
+            else throw new TimeoutException();
+        }
+    }
+}
